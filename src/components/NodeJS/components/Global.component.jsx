15a7c2f4b3cf0ea6fd984f80508b4d7119bded19
Jarett Young <know_jarett@yahoo.com>
2022-08-25 19:53:30 -0500

    adding content to NodeJs Section

--- src/components/NodeJS/components/Global.component.jsx
+++ src/components/NodeJS/components/Global.component.jsx
@@ -1,4 +1,12 @@
 import { DataTable, CodeBlock, Footer } from "../../index";
+
+const code1 = `
+  setTimeout(() => {
+      console.log('hello');
+  }, 5000)
+
+`;
+
 const Global = () => {
   return (
     <div className="home-section color6">
@@ -6,6 +14,24 @@ const Global = () => {
         <span className="title">Node.js Global Object</span>
       </div>
       <div className="home-content">
+        <p>
+          In Node, we have a <span className="code-span">global</span> object
+          that we can always access. Features that we expect to be available
+          everywhere live in this <span className="code-span">global</span>{" "}
+          object.
+        </p>
+        <p>
+          For example, to have some code executed after 5 seconds we can use
+          either <span className="code-span">global.setTimeout</span> or just{" "}
+          <span className="code-span">setTimeout</span>. The{" "}
+          <span className="code-span">global</span> keyword is optional.
+        </p>
+        <CodeBlock code={code1} />
+        <p>
+          Probably the most famous global is{" "}
+          <span className="code-span">global.console.log</span> which we write
+          as just <span className="code-span">console.log</span>.
+        </p>
         <div className="section-header">Heading 1</div>
         <p>Paragraph1</p>
         <p>You can also have function expressions as elements of an array</p>
