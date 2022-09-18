4aa45c1e47320bd74689925eb7c0d18c6742b10e
bluejet80 <know_jarett@yahoo.com>
2022-09-17 20:26:22 -0500

    finished git section

--- src/components/General/components/Git.component.jsx
+++ src/components/General/components/Git.component.jsx
@@ -33,6 +33,29 @@ const Git = () => {
         <p className="highlight">git remote add origin (link to repo)</p>
         <p>Then do a pull to get the README.md file.</p>
         <p className="highlight">git pull origin master</p>
+        <div className="section-header">Git Branches</div>
+        <p>
+          To create a branch off of the branch that you are currently on type:
+        </p>
+        <p className="highlight">git branch new-branch-name</p>
+        <p>To delete a branch type</p>
+        <p className="highlight">
+          git branch -d branch-name (or -D to force a delete)
+        </p>
+        <p>To display all local and remote-tracking branches type</p>
+        <p className="highlight">git branch -a</p>
+
+        <h3>Switching Branches</h3>
+        <p>
+          To switch branches you will use the checkout command. This command can
+          do other things as well like create new branches
+        </p>
+        <p className="highlight">git checkout branch-name</p>
+        <p>
+          To create a new branch off of the current branch and switch to that
+          branch type:
+        </p>
+        <p className="highlight">git checkout -b branch-name</p>
         <div className="section-header">Git Commits</div>
         <p>
           You begin a commit by first adding all the changed files to the
@@ -55,9 +78,68 @@ const Git = () => {
         </p>
         <p>Similiarly you can combine the two first commands by typing.</p>
         <p className="highlight">git commit -a -m "commit message"</p>
-        <p>If you want to commit only certain files you can type</p>
-        <div className="section-header">Show history of commits</div>
+        <p>
+          If you want to commit only certain files you can type. First you need
+          to see what files have been changed. You do this by running:
+        </p>
+        <p className="highligh">git status</p>
+        <p>Then you can only commit certain files by doing this</p>
+        <p className="highlight">git add (file1) (file2)</p>
+        <p>Then you just do a commit as normal after that.</p>
 
+        <div className="section-header">Show history of commits</div>
+        <p>
+          To get information about the commits that you have done just type:
+        </p>
+        <p className="highlight">git log</p>
+        <p>The prefered shorter version of the command above is this:</p>
+        <p className="highlight">git log --oneline</p>
+        <p>
+          After running this you get a nice consice list of all the commits and
+          their respective hashes. All you need.
+        </p>
+        <p>
+          To see a graph of the merges that you have made with your commits just
+          type:
+        </p>
+        <p className="highlight">git log --graph</p>
+
+        <div className="section-header">Git Reset</div>
+        <p>
+          Git reset is used to unstage files or remove commits. It does so by
+          changing what the tip of the branch (HEAD) points to.
+        </p>
+        <p>There are three reset modes: --soft, --mixed(default), --hard</p>
+        <p>To undo changes of a file or a staging,you would do:</p>
+        <p className="highlight">git add .</p>
+        <p className="highlight">git reset -- file (unstage a single file)</p>
+        <p className="highlight">git reset (unstage all)</p>
+        <p>Then you type this to undo changes to the file</p>
+        <p className="highlight">git restore (file)</p>
+        <p>
+          If you wish to undo the most recent commit, then you woul do this:
+        </p>
+        <p className="highlight">git commit -a -m "commit message"</p>
+        <p className="highlight">git reset --soft HEAD^</p>
+        <p className="highlight">git commit -a -m "new commit message"</p>
+        <p>To undo a commit permanently:</p>
+        <p className="highlight">
+          git reset --hard HEAD~2(destroys the last two commits, HEAD and HEAD^)
+        </p>
+        <div className="section-header">Git Stash</div>
+        <p>
+          Stashing allows you to save your curent unstages changes and bring
+          your branch back to an unmodified state. When you stash, your changes
+          are pushed onto a stack. This is really useful if you need to switch
+          to another branch whithout having to commit your incomplete changes.
+        </p>
+        <p className="highlight">git stash (stash your unstaged changes)</p>
+        <p className="highlight">git stash pop (unstash your changes)</p>
+        <p className="highlight">git stash drop (drops your stashed changes)</p>
+        <p className="highlight">git stash push file (stash a single file)</p>
+        <p className="highlight">
+          git stash -m "message" (add a message to your stash)
+        </p>
         <div className="section-header">Saving Password Locally</div>
         <p>
           To not have to type in your appsword every time you do a push you can
@@ -71,7 +153,7 @@ const Git = () => {
           process over again.
         </p>
 
-        <Footer complete={false} />
+        <Footer complete={true} />
       </div>
     </div>
   );
--- src/components/General/components/Planning.component.jsx
+++ src/components/General/components/Planning.component.jsx
@@ -7,10 +7,18 @@ const Planning = () => {
         <span className="title">App Planning Information</span>
       </div>
       <div className="home-content white-text">
-        <div className="section-header">Heading 1</div>
-        <p>Paragraph1</p>
+        <div className="section-header">Creating a Project Flowchart</div>
+        <p>
+          There is a great website that allows you to create a flow chart for
+          your project it is here:{" "}
+          <a href="https://app.diagrams.net/">Diagrams.net website</a>
+        </p>
 
-        <p>You can also have function expressions as elements of an array</p>
+        <p>
+          From here I want to also have links to the various worksheet PDFs that
+          I have made.
+        </p>
+
         <Footer complete={false} />
       </div>
     </div>
